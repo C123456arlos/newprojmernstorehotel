8c9cb9d6cc2960652d4fea59690e367f824c83e9
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-08-20 12:21:17 -0400

    first commit

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import hotelRouter from './routes/hotelRoutes.js'
 import connectCloudinary from './configs/cloudinary.js'
 import roomRouter from './routes/roomRoutes.js'
 import bookingRouter from './routes/bookingRoutes.js'
+import { stripeWebhooks } from './controllers/stripeWebhooks.js'
 
 connectDB()
 connectCloudinary()
@@ -16,6 +17,7 @@ connectCloudinary()
 
 const app = express()
 app.use(cors())
+app.post('/api/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 app.use(express.json())
 app.use(clerkMiddleware())
 app.use('/api/clerk', clerkWebhooks)
